12f5faec12ef137930b0599756446aea4b0ab413
userisro <userisro@gmail.com>
2017-12-17 22:37:08 +0530

    Add ability to download new content from courses.

--- cms_scraper.py
+++ cms_scraper.py
@@ -25,12 +25,12 @@ def get_config(path):
 def read_file(path, no_data, data_handler):
 	"""Properly handle reading a file."""
 	if not os.path.isfile(path):
-		print(f'{path} not found!', end=' ')
+		print(f'{path} not found!')
 		return no_data()
 	with open(path, 'r') as f:
 		data = f.read()
 		if not data:  # in case the file is empty
-			print(f'{path} is empty.', end=' ')
+			print(f'{path} is empty.')
 			return no_data()
 		return data_handler(data)
 
@@ -49,11 +49,13 @@ def get_attr(text, param, offset=0, end_ch='"'):
 
 
 def make_fold(parent, name):
+	"""Create a directory from Path object."""
 	for ch in ['\\', '/']:
 		name = name.replace(ch, ' ')
 	folder = parent / name
 	if not folder.is_dir():
 		folder.mkdir()
+		print(f'Created {folder!s}.')
 	return folder
 
 
@@ -167,7 +169,7 @@ def fold_contents(fold_url):
 	return contents
 
 
-def get_folders(src):
+def get_folders(src, existing_contents=[]):
 	"""Get all folders, with their contents, from a course/folder."""
 	folders = src.find_all('li', {'class': 'activity folder modtype_folder '})
 	folds = []
@@ -175,47 +177,70 @@ def get_folders(src):
 		link = fold.find('a')
 		fold_name = link.span.find(text=True, recursive=False)
 		fold_url = link['href']
-		fold_id = get_attr(fold_url, 'id=', 3)
-
+		fold_id = int(get_attr(fold_url, 'id=', 3))
+		if fold_id in existing_contents:  # skip the folder if already in db
+			continue
 		contents = fold_contents(fold_url)
 		folder_data = {
 			'name': fold_name,
 			'type': 'folder',
 			'id': fold_id,
+			'downloaded': False,
 			'contents': contents
 		}
 		folds.append(folder_data)
 	return folds
 
 
-def get_course_links(c_id):
+def get_files(src, existing_contents=[]):
+	"""Get all the file links from a course page."""
+	files = src.find_all('li', {'class': 'activity resource modtype_resource '})
+	f = []
+	for file in files:
+		link = file.find('a')
+		name = link.span.find(text=True, recursive=False)
+		file_id = int(get_attr(link['href'], 'id=', 3))
+		if file_id in existing_contents:  # skip the file if already in db
+			continue
+		file_data = {
+			'name': name,
+			'type': 'file',
+			'id': file_id,
+			'downloaded': False
+		}
+		f.append(file_data)
+	return f
+
+
+def get_course_links(c_id, course):
 	"""Get link of each file for a course."""
 	c_url = moodle_url + 'course/view.php'
 	r = sess.get(c_url, params={'id': c_id})
 	soup = BeautifulSoup(r.text, 'html.parser')
-	course_name = soup.find('h3', {'class': 'page-subtitle'}).text
+
+	course['name'] = soup.find('h3', {'class': 'page-subtitle'}).text
+	print(f'Getting links for {course["name"]}.')
+
+	existing_contents = [content['id'] for content in course['contents']]
+	course['contents'].extend(get_folders(soup, existing_contents))  # the folders in a course are traversed here
+	course['contents'].extend(get_files(soup, existing_contents))
+
+
+def read_course(c_id, db):
+	"""Get the course from database, else return a new course."""
+	for course in db:
+		if c_id == str(course['id']):
+			return course
+
 	course = {
-		'name': course_name,
+		'name': '',
 		'type': 'course',
 		'id': int(c_id),
 		'remain enrolled': 0,
 		'contents': []
 	}
-	course['contents'].extend(get_folders(soup))  # the folders in a course are traversed here
-
-	files = soup.find_all('li', {'class': 'activity resource modtype_resource '})
-	for file in files:
-		link = file.find('a')
-		name = link.span.find(text=True, recursive=False)
-		file_data = {
-			'name': name,
-			'type': 'file',
-			'id': get_attr(link['href'], 'id=', 3),
-			'downloaded': False,
-			'url': link['href']
-		}
-		course['contents'].append(file_data)
-	return course
+	db.append(course)  # add the new course to the database
+	return read_course(c_id, db)
 
 
 def update_db():
@@ -223,21 +248,17 @@ def update_db():
 	print("Updating database.")
 	ids = read_file('all_ids.txt', get_all_courses, lambda d: d.split('\n'))
 	db = read_file('courses_db.json', lambda: [], lambda d: json.loads(d))
-	ids = ['1263']
-	for c_id in ids:
-		for course in db:
-			if c_id == str(course['id']):
-				ids.remove(c_id)
+
 	for c_id in ids:  # TODO: Break up enrolment into small groups.
 		remain_enrolled = course_enrol(c_id)
 		if remain_enrolled is -1:  # in case enrollment was unsuccessful
 			continue
-		course_data = get_course_links(c_id)
-		course_data["remain enrolled"] = remain_enrolled
-		db.append(course_data)  # add the new course links to the database
+		course = read_course(c_id, db)
+		get_course_links(c_id, course)
+		course["remain enrolled"] = remain_enrolled
 
-	with open('courses_db.json', 'w') as f:
-		f.write(json.dumps(db, indent=4))
+		with open('courses_db.json', 'w') as f:
+			f.write(json.dumps(db, indent=4))
 
 
 def extract_archive(archive_path):
@@ -249,9 +270,11 @@ def extract_archive(archive_path):
 		archive = rarfile.RarFile(str(archive_path), 'r')
 
 	folder = make_fold(archive_path.parent, archive_path.stem)
+	print('Extracting the archive.', end=' ')
 	archive.extractall(str(folder))
 	archive.close()
-	# archive_path.unlink()  # delete the archive
+	archive_path.unlink()  # delete the archive
+	print('Done.')
 
 	contents = list(folder.iterdir())
 	if len(contents) == 1 and contents[0].is_dir():  # in case the archive only contained one folder
@@ -264,13 +287,19 @@ def extract_archive(archive_path):
 
 def download_file(file, folder):
 	"""Where the actual downloading happens."""
-	file_url = file['url']
+	if 'url' in file:  # for files inside folders
+		file_url = file['url']
+	else:  # for files directly inside course directory
+		file_url = moodle_url + "mod/resource/view.php?id=" + str(file['id'])
+
 	r = sess.get(file_url, stream=True)
+
 	try:
 		file_name = get_attr(r.headers['Content-Disposition'], '="', 2, '"')
 	except KeyError:
 		print('Unable to access file. Check if you are logged in and enrolled.')
 		return
+
 	print(f"Downloading {file_name}.", end=' ')
 	file['real_name'] = file_name  # this is the name by which file is saved
 	file_path = folder / file_name
@@ -283,22 +312,24 @@ def download_file(file, folder):
 		print('Done.')
 
 	if any([file_path.suffix == ext for ext in ['.zip', '.rar']]):
-		print('Extracting the archive.', end=' ')
 		extract_archive(file_path)
-		print('Finished.')
+
+	if 'url' in file:
+		del file['url']
 
 
 def download_contents(contents, fold):
 	"""Download files of a course/folder."""
 	for content in contents:
+		if content['downloaded']:  # to avoid redownloading the file/folder
+			print(f"Already downloaded {content['name']}.")
+			continue
 		if content['type'] == 'folder':
 			new_fold = make_fold(fold, content['name'])
 			download_contents(content['contents'], new_fold)  # Recursively traverse the folders in case of sub-directories.
 		elif content['type'] == 'file':
-			if content['downloaded']:  # to avoid redownloading the file
-				print(f"Already downloaded {content['name']}.")
-				continue
 			download_file(content, fold)
+		content['downloaded'] = True
 
 
 def download():
@@ -314,8 +345,8 @@ def download():
 		if not course['remain enrolled']:
 			course_unenrol(course['id'])
 
-	with open('courses_db.json', 'w') as f:
-		f.write(json.dumps(courses, indent=4))
+		with open('courses_db.json', 'w') as f:
+			f.write(json.dumps(courses, indent=4))
 
 
 def main():
